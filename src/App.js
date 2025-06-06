b1a346229840790498bdad950e7fb8849a44e11d
Villy Siu <villysiu@gmail.com>
2025-06-06 11:36:58 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         Cat Lover
-      </header>
+      </header> */}
+      <NavBar />
     </div>
   );
 }

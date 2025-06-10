55265b1e1c99c778f124b813694baaae180afb7b
Villy Siu <villysiu@gmail.com>
2025-06-10 09:46:04 -0700

    fetch all data in Mainbody to avoid rerendering

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,33 +1,64 @@
 import {favData} from './data';
 import {useState, useEffect} from 'react';
 import Random from './Random'
+import Favorites from './Favorites'
+import Spinner from 'react-bootstrap/Spinner';
+
 
 const MainBody = ({choice}) =>{
 
-    const [favs, setFavs] = useState([]);
+    const [status, setStatus] = useState('idle');
+    const [favorites, setFavorties] = useState([]);
+    const [randoms, setRandoms] = useState([]);
 
     useEffect(()=>{
-        async function getFavorites(){
-            setFavs([]);
-            // try {
-            //     const response = await fetch('', {
-            //         headers: {
-            //             'content-type': 'application/json',
-            //             'x-api-key': process.env.REACT_APP_CAT_API_KEY,
-            //         }
-            //     });
-            //     const data = await response.json();
-            //     setImages(data);
+        if(status === 'idle'){
+            async function fetchData(){
+                
+                try {
+                    const response = await fetch('https://api.thecatapi.com/v1/favourites', {
+                        headers: {
+                            'content-type': 'application/json',
+                            'x-api-key': process.env.REACT_APP_CAT_API_KEY,
+                        }
+                    });
+    
+                    setFavorites(await response.json());
 
-            // } catch(error){
-            //     console.log(error);
-            // }
-            setFavs(favData);
+                    const response2 = await fetch('https://api.thecatapi.com/v1/images/search?limit=12&has_breeds=1', {
+                        headers: {
+                            'content-type': 'application/json',
+                            'x-api-key': process.env.REACT_APP_CAT_API_KEY,
+                        }
+                    });
+                    
+                    setRandoms(await response2.json());
+                    
+                    setStatus('succeeded')
+
+                } catch(error){
+                    console.log(error);
+                    setStatus('failed');
+                }
+                
+            }
+            setStatus('loading');
+            fetchData();
         }
-        getFavorites();
-    }, [])
+        
+    }, [status])
+
+    if(status === 'idle' || status === 'loading'){
+        return <Spinner />
+    }
+    if(status === 'failed'){
+        return <div>failed to fetch data</div>
+    }
     return(
-        <Random favs={favs} setFavs={setFavs} />
-    )
+        <>
+        <Random randoms={randoms} setFavorites={setFavorites} />
+         {/* {choice === 0 ? <Random favorites={favorites} setFavorites={setFavorites} /> : <Favorites favorites={favorites} setFavorites={setFavorites} />} */}
+        </>
+    ) 
 }
 export default MainBody
